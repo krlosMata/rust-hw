588f9af4786770218d972f2739f9f22f0ff53aba
krlosMata <krlosmata@gmail.com>
2019-01-19 18:17:02 +0100

    Revert "hello cargo exercise"
    This reverts commit ed04b454358f7f0b0c0ba7211015be023c5a275a.

--- hw00/src/main.rs
+++ hw00/src/main.rs
@@ -1,4 +1,3 @@
 fn main() {
-    let name = "Ferris"; // Ferris is the name of Rust's unofficial crustacean mascot
-    println!("Hello, {}!", name);
+    println!("Hello, world!");
 }
--- hw00/target/debug/hw00.d
+++
@@ -1,1 +0,0 @@
-/home/ixnay/ixnayDev/Rust/rust-hw/hw00/target/debug/hw00: /home/ixnay/ixnayDev/Rust/rust-hw/hw00/src/main.rs
